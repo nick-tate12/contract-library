65dde8f023be28cdd04bc72ae76f192336fc8d41
nick-tate12 <nickatate12@yahoo.com>
2023-04-12 20:10:12 -0500

    more base repo setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './config'; // Load environment variables
 import 'express-async-errors'; // Enable default error handling for async errors
 import express, { Express } from 'express';
+import session from 'express-session';
+import connectSqlite3 from 'connect-sqlite3';
 import { registerUser, logIn, getAllUsers, updateUserEmail } from './controllers/UserController';
 import { addNewContract, getAllContracts } from './controllers/ContractController';
 import { addNewFarmer, getAllFarmers } from './controllers/FarmerController';
@@ -9,10 +11,26 @@ import { addNewBuyer, getAllBuyers } from './controllers/BuyerController';
 import { addNewCrop, getAllCrops } from './controllers/CropController';
 
 const app: Express = express();
-// app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.set('view engine', 'ejs');
+const { PORT, COOKIE_SECRET } = process.env;
 
-const { PORT } = process.env;
+const SQLiteStore = connectSqlite3(session);
+const store = new SQLiteStore({ db: 'session.sqlite' });
+
+app.use(express.static('public', { extensions: ['html'] }));
+
+app.use(
+  session({
+    store,
+    secret: COOKIE_SECRET || '',
+    cookie: { maxAge: 8 * 60 * 60 * 1000 }, // 8 hours
+    name: 'session',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // the 'user' will be 'marketers' for Delta Grain Marketing
 app.post('/api/users', registerUser); // Create an account
---
+++ src/types/express-session.d.ts
@@ -0,0 +1,13 @@
+import 'express-session';
+
+declare module 'express-session' {
+  export interface Session {
+    clearSession(): Promise<void>; // DO NOT MODIFY THIS!
+    authenticatedUser: {
+      userId: string;
+      email: string;
+    };
+    isLoggedIn: boolean;
+    logInTimeout: Date;
+  }
+}
